8c9f58de5b45bf43c9feacbc89affb65c3101e95
Yeasar <yeasar1507099@stud.kuet.ac.bd>
2023-07-29 16:57:16 +0600

    linked list with python

---
+++ Data_Structure/linked_list_python.py
@@ -0,0 +1,104 @@
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_first(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def insert_at_last(self, data):
+        node = Node(data)
+
+        if self.head is None:
+            self.head = node
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = node
+
+    def get_length(self):
+        cnt = 0
+        itr = self.head
+        while itr:
+            cnt += 1
+            itr = itr.next
+        return cnt
+
+    def insert_at_pos(self, pos, data):
+        if pos > self.get_length():
+            return "out of range."
+        elif pos == self.get_length():
+            self.insert_at_last(data)
+            return
+        if pos == 0:
+            self.insert_at_first(data)
+            return
+
+        cnt = -1
+        itr = self.head
+        prev = None
+        while itr:
+            cnt += 1
+            if(cnt == pos):
+                break
+            prev = itr
+            itr = itr.next
+        node = Node(data)
+        node.next = prev.next
+        prev.next = node
+
+    def delete_at(self, pos):
+        if self.get_length() <= pos:
+            return "out of range."
+        if (pos == 0):
+            itr = self.head
+            if itr.next is None:
+                self.head = None
+            else:
+                self.head = itr.next
+            return
+        cnt = -1
+        itr = self.head
+        prev = None
+        while itr:
+            cnt += 1
+            if(cnt == pos):
+                break
+            prev = itr
+            itr = itr.next
+        prev.next = itr.next
+
+
+
+    def display(self):
+        if self.head == None:
+            print("empty")
+            return
+        itr = self.head
+        llstr = ""
+        while itr:
+            llstr += str(itr.data) + " -> "
+            itr = itr.next
+        print(llstr)
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_at_first(4)
+    ll.insert_at_first(34)
+    ll.insert_at_last(99)
+    ll.insert_at_pos(0, 4)
+    ll.insert_at_pos(ll.get_length(), 999)
+    print(ll.get_length())
+    ll.display()
+    ll.insert_at_pos(2, 343)
+    ll.delete_at(0)
+    ll.delete_at(ll.get_length() - 1)
+    print(ll.get_length())
+    ll.display()

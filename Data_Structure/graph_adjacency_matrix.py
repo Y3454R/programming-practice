f98123e265422443e470d2de549ab4bd9e16418b
Samin Yeasar Abir <63140759+Y3454R@users.noreply.github.com>
2023-12-24 12:46:39 +0600

    Update graph_adjacency_matrix.py

--- Data_Structure/graph_adjacency_matrix.py
+++ Data_Structure/graph_adjacency_matrix.py
@@ -1,3 +1,5 @@
+# https://www.shafaetsplanet.com/planetcoding/?p=184
+
 # 3 3  //3 nodes and 3 edges
 # 1 2 5 //node1-node2-cost
 # 2 3 8
@@ -12,7 +14,7 @@ for e in range(edge):
     adj_mat[node1][node2] = cost
     adj_mat[node2][node1] = cost
 
-for i in range(1, node+1):
+for i in range(1, node + 1):
     for j in range(1, node + 1):
         print(adj_mat[i][j], end=" ")
     print()
@@ -20,3 +22,4 @@ for i in range(1, node+1):
 # The line adj_mat = [[0 for i in range(node + 1)]] * (node + 1) creates a list with node + 1 references to the same inner list [0 for i in range(node + 1)].
 # This is a shallow copy, meaning that if you modify one of the inner lists, the changes will be reflected in all the other inner lists
 # because they reference the same list object. adj_mat = [[0 for i in range(node + 1)] for j in range(node + 1)]
+

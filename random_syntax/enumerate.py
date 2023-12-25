2ce4892f1bc22f77a3e830eb5af8782e69d6ba2d
Samin Yeasar Abir <63140759+Y3454R@users.noreply.github.com>
2023-12-25 15:30:36 +0600

    Create enumerate.py

---
+++ random_syntax/enumerate.py
@@ -0,0 +1,13 @@
+my_list = [98, 99, 111]
+my_dick = {}
+for indx, element in enumerate(my_list):
+    my_dick[indx] = element
+
+print(my_dick)
+
+my_dick2 = {}
+
+for indx, element in enumerate(my_list):
+    my_dick2[element] = False
+
+print(my_dick2)
